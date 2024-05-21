6759312ba7750b21982014b7424cafd9ffeff981
VICTORxEVO <yassirsbai04@gmail.com>
2024-05-21 16:24:55 +0100

    Final V0.89

--- source/pipex.c
+++ source/pipex.c
@@ -6,7 +6,7 @@
 /*   By: ysbai-jo <ysbai-jo@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 11:07:53 by ysbai-jo          #+#    #+#             */
-/*   Updated: 2024/05/21 16:03:20 by ysbai-jo         ###   ########.fr       */
+/*   Updated: 2024/05/21 16:23:49 by ysbai-jo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,10 +26,7 @@ int main(int ac, char *av[], char *env[])
         if (pid < 0)
             (destroy(&core), peexit("pipex: ", 1, 'P', false));
         if (!pid)
-        {
-            printf("pid is %d in %d\n", pid, core.i);
             child_action(&core);
-        }
         else
         {
             core.pids[core.i] = pid;
